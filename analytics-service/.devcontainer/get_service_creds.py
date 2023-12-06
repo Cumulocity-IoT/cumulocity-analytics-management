c4e0697c2377e7e7d55fd6f5c811ec0f1fc5e743
Marco Stoffel <marco.stoffel@softwareag.com>
2023-12-06 13:14:03 +0100

    Update Readme for MultiTenant MS

--- analytics-service/.devcontainer/get_service_creds.py
+++ analytics-service/.devcontainer/get_service_creds.py
@@ -6,9 +6,9 @@ credentials of a corresponding microservice registered at Cumulocity."""
 base_url, tenant, user, password = ms_util.get_bootstrap_credentials('analytics-ext-service')
 with open(f'.env', 'w', encoding='UTF-8') as f:
     f.write(f'C8Y_BASEURL={base_url}\n'
-            f'C8Y_TENANT={tenant}\n'
-            f'C8Y_USER={user}\n'
-            f'C8Y_PASSWORD={password}\n')
+            f'C8Y_BOOTSTRAP_TENANT={tenant}\n'
+            f'C8Y_BOOTSTRAP_USER={user}\n'
+            f'C8Y_BOOTSTRAP_PASSWORD={password}\n')
 
 
 
