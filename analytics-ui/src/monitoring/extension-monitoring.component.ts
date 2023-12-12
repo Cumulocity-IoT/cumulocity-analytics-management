617ab4afaf980f740aacf8e76d48180b717572f8
sagIoTPower <christof.strack@softwareag.com>
2023-12-12 10:58:48 +0100

    changed styling

--- analytics-ui/src/monitoring/extension-monitoring.component.ts
+++ analytics-ui/src/monitoring/extension-monitoring.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Output, ViewEncapsulation } from "@angular/core";
 import { BehaviorSubject, Observable, Subject, of } from "rxjs";
-import { AlarmService, IAlarm, IResultList } from "@c8y/client";
+import { AlarmService, AlarmStatus, IAlarm, IResultList } from "@c8y/client";
 import { shareReplay, switchMap, tap } from "rxjs/operators";
 import { BsModalRef } from "ngx-bootstrap/modal";
 import { AnalyticsService } from "../shared/analytics.service";
@@ -11,13 +11,14 @@ import { AnalyticsService } from "../shared/analytics.service";
   encapsulation: ViewEncapsulation.None,
 })
 export class ExtensionMonitoringComponent implements OnInit {
-
   cepId: string;
   @Output() closeSubject: Subject<void> = new Subject();
   alarms$: Observable<IResultList<IAlarm>>;
   nextPage$: BehaviorSubject<any> = new BehaviorSubject({ direction: 0 });
   currentPage: number = 1;
   searchString: string;
+  status: AlarmStatus;
+  AlarmStatus = AlarmStatus;
 
   constructor(
     private alarmService: AlarmService,
@@ -36,9 +37,14 @@ export class ExtensionMonitoringComponent implements OnInit {
     };
     this.alarms$ = this.nextPage$.pipe(
       tap((options) => {
-        this.currentPage = this.currentPage + options.direction;
-        if (this.currentPage < 1) this.currentPage = 1;
-        filter["currentPage"] = this.currentPage;
+        if (options.direction) {
+          this.currentPage = this.currentPage + options.direction;
+          if (this.currentPage < 1) this.currentPage = 1;
+          filter["currentPage"] = this.currentPage;
+        }
+        if (options.status) {
+          filter["status"] = options.status;
+        }
       }),
       switchMap(() => this.alarmService.list(filter)),
       shareReplay()
@@ -51,6 +57,10 @@ export class ExtensionMonitoringComponent implements OnInit {
   }
 
   nextPage(direction: number) {
-    this.nextPage$.next({direction});
+    this.nextPage$.next({ direction });
+  }
+
+  search() {
+    this.nextPage$.next({ status:this.status });
   }
 }
