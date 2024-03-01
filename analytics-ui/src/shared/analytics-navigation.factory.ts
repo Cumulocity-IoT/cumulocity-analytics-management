d082b39c6ace8b191ebaa6a2013833b925b35b46
sagIoTPower <christof.strack@softwareag.com>
2024-03-01 14:58:38 +0100

    raise version

--- analytics-ui/src/shared/analytics-navigation.factory.ts
+++ analytics-ui/src/shared/analytics-navigation.factory.ts
@@ -2,14 +2,19 @@ import { Injectable } from '@angular/core';
 import { ApplicationService } from '@c8y/client';
 
 import {
+    AlertService,
   gettext,
   NavigatorNode,
-  NavigatorNodeFactory,
+  NavigatorNodeFactory
 } from '@c8y/ngx-components';
+import { APPLICATION_ANALYTICS_BUILDER_SERVICE } from './analytics.model';
 
 @Injectable()
 export class AnalyticsNavigationFactory implements NavigatorNodeFactory {
-  constructor(private applicationService: ApplicationService) {}
+  constructor(
+    private applicationService: ApplicationService,
+    private alertService: AlertService
+  ) {}
 
   get() {
     const navs: NavigatorNode[] = [];
@@ -22,6 +27,20 @@ export class AnalyticsNavigationFactory implements NavigatorNodeFactory {
       preventDuplicates: true
     });
     navs.push(extensionsNode);
-    return navs;
+
+    return this.applicationService
+      .isAvailable(APPLICATION_ANALYTICS_BUILDER_SERVICE)
+      .then((data) => {
+        if (!data.data ) {
+          this.alertService.add({
+            text: 'Microservice: <code>analytics-ext-service</code> not subscribed. Please subscribe this service before using the analytics plugin!',
+            allowHtml: true,
+            type: 'warning'
+          });
+          console.error('analytics-ext-service not subscribed!');
+          return [];
+        }
+        return navs;
+      });
   }
 }
